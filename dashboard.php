157685514e7d3ce7dfa82fccae92e33646e211eb
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-07 00:21:30 +0500

    replaced dummy with relevant content

--- dashboard.php
+++ dashboard.php
@@ -26,7 +26,7 @@
             <!-- TABLE: LATEST ORDERS -->
             <div class="card">
               <div class="card-header border-transparent">
-                <h3 class="card-title">Latest Orders</h3>
+                <h3 class="card-title">Latest Products</h3>
 
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -43,67 +43,67 @@
                   <table class="table m-0">
                     <thead>
                     <tr>
-                      <th>Order ID</th>
+                      <th>Product ID</th>
                       <th>Item</th>
                       <th>Status</th>
-                      <th>Popularity</th>
+                      <th>Category</th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
-                      <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                      <td>Call of Duty IV</td>
+                      <td><a href="pages/examples/invoice.html">P9842</a></td>
+                      <td>Cat food</td>
                       <td><span class="badge badge-success">Shipped</span></td>
                       <td>
-                        <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+                        <div class="sparkbar" data-color="#00a65a" data-height="20">Food accessories</div>
                       </td>
                     </tr>
                     <tr>
-                      <td><a href="pages/examples/invoice.html">OR1848</a></td>
-                      <td>Samsung Smart TV</td>
+                      <td><a href="pages/examples/invoice.html">P1848</a></td>
+                      <td>German Shephard</td>
                       <td><span class="badge badge-warning">Pending</span></td>
                       <td>
-                        <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
+                        <div class="sparkbar" data-color="#f39c12" data-height="20">Pet</div>
                       </td>
                     </tr>
                     <tr>
-                      <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                      <td>iPhone 6 Plus</td>
+                      <td><a href="pages/examples/invoice.html">P7429</a></td>
+                      <td>Black mamba</td>
                       <td><span class="badge badge-danger">Delivered</span></td>
                       <td>
-                        <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
+                        <div class="sparkbar" data-color="#f56954" data-height="20">Reptiles</div>
                       </td>
                     </tr>
                     <tr>
-                      <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                      <td>Samsung Smart TV</td>
+                      <td><a href="pages/examples/invoice.html">P7429</a></td>
+                      <td>Fringe Bird</td>
                       <td><span class="badge badge-info">Processing</span></td>
                       <td>
-                        <div class="sparkbar" data-color="#00c0ef" data-height="20">90,80,-90,70,-61,83,63</div>
+                        <div class="sparkbar" data-color="#00c0ef" data-height="20">Birds</div>
                       </td>
                     </tr>
                     <tr>
-                      <td><a href="pages/examples/invoice.html">OR1848</a></td>
-                      <td>Samsung Smart TV</td>
+                      <td><a href="pages/examples/invoice.html">P1848</a></td>
+                      <td>cheetah baby</td>
                       <td><span class="badge badge-warning">Pending</span></td>
                       <td>
-                        <div class="sparkbar" data-color="#f39c12" data-height="20">90,80,-90,70,61,-83,68</div>
+                        <div class="sparkbar" data-color="#f39c12" data-height="20">Wild</div>
                       </td>
                     </tr>
                     <tr>
-                      <td><a href="pages/examples/invoice.html">OR7429</a></td>
-                      <td>iPhone 6 Plus</td>
+                      <td><a href="pages/examples/invoice.html">P7429</a></td>
+                      <td>Gold fish</td>
                       <td><span class="badge badge-danger">Delivered</span></td>
                       <td>
-                        <div class="sparkbar" data-color="#f56954" data-height="20">90,-80,90,70,-61,83,63</div>
+                        <div class="sparkbar" data-color="#f56954" data-height="20">Aquarious</div>
                       </td>
                     </tr>
                     <tr>
-                      <td><a href="pages/examples/invoice.html">OR9842</a></td>
-                      <td>Call of Duty IV</td>
+                      <td><a href="pages/examples/invoice.html">P9842</a></td>
+                      <td>Pi-Iodene</td>
                       <td><span class="badge badge-success">Shipped</span></td>
                       <td>
-                        <div class="sparkbar" data-color="#00a65a" data-height="20">90,80,90,-70,61,-83,63</div>
+                        <div class="sparkbar" data-color="#00a65a" data-height="20">Vaccine</div>
                       </td>
                     </tr>
                     </tbody>
@@ -144,15 +144,7 @@
               <!-- /.info-box-content -->
             </div>
             <!-- /.info-box -->
-            <div class="info-box mb-3 bg-danger">
-              <span class="info-box-icon"><i class="fas fa-cloud-download-alt"></i></span>
-
-              <div class="info-box-content">
-                <span class="info-box-text">Downloads</span>
-                <span class="info-box-number">114,381</span>
-              </div>
-              <!-- /.info-box-content -->
-            </div>
+    
             <!-- /.info-box -->
             <div class="info-box mb-3 bg-info">
               <span class="info-box-icon"><i class="far fa-comment"></i></span>
@@ -188,10 +180,10 @@
                       <img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
                     </div>
                     <div class="product-info">
-                      <a href="javascript:void(0)" class="product-title">Samsung TV
+                      <a href="javascript:void(0)" class="product-title">Bird Cage
                         <span class="badge badge-warning float-right">$1800</span></a>
                       <span class="product-description">
-                        Samsung 32" 1080p 60Hz LED Smart HDTV.
+                        Category : Home Acessories
                       </span>
                     </div>
                   </li>
@@ -201,10 +193,10 @@
                       <img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
                     </div>
                     <div class="product-info">
-                      <a href="javascript:void(0)" class="product-title">Bicycle
+                      <a href="javascript:void(0)" class="product-title">Meow Food
                         <span class="badge badge-info float-right">$700</span></a>
                       <span class="product-description">
-                        26" Mongoose Dolomite Men's 7-speed, Navy Blue.
+                        Category: Food Acessories
                       </span>
                     </div>
                   </li>
@@ -215,12 +207,12 @@
                     </div>
                     <div class="product-info">
                       <a href="javascript:void(0)" class="product-title">
-                        Xbox One <span class="badge badge-danger float-right">
-                        $350
+                        Pit bull <span class="badge badge-danger float-right">
+                        $3500
                       </span>
                       </a>
                       <span class="product-description">
-                        Xbox One Console Bundle with Halo Master Chief Collection.
+                        Category: Pet
                       </span>
                     </div>
                   </li>
@@ -230,10 +222,10 @@
                       <img src="dist/img/default-150x150.png" alt="Product Image" class="img-size-50">
                     </div>
                     <div class="product-info">
-                      <a href="javascript:void(0)" class="product-title">PlayStation 4
+                      <a href="javascript:void(0)" class="product-title">Scorpio
                         <span class="badge badge-success float-right">$399</span></a>
                       <span class="product-description">
-                        PlayStation 4 500GB Console (PS4)
+                        Category: Vaccine
                       </span>
                     </div>
                   </li>
