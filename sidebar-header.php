d9b556d9f5239de8b40a1ab95908edd3558260c5
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-07 00:49:05 +0500

    make dashboard links working

--- sidebar-header.php
+++ sidebar-header.php
@@ -93,7 +93,7 @@ session_start();
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">Pak Animals</span>
     </a>
 
     <!-- Sidebar -->
@@ -114,14 +114,17 @@ session_start();
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
+
             <ul class="nav nav-treeview">
+              <li class="nav-item active">
+                <a href="./dashboard.php" class="nav-link active">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                  Dashboard
+                  
+                </p>
+              </a>
+              </li>
               <li class="nav-item">
                 <a href="./product.php" class="nav-link">
                   <i class="fa fa-plus nav-icon"></i>
@@ -156,12 +159,12 @@ session_start();
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Dashboard v2</h1>
+            <h1 class="m-0 text-dark">Dashboard</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v2</li>
+              <li class="breadcrumb-item active">Dashboard</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
