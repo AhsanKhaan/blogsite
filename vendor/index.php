aafc3ca6941c77583aaa5a0ed211cdf06708e65d
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-07 11:20:40 +0500

    changed user interface of vendor

--- vendor/index.php
+++ vendor/index.php
@@ -54,16 +54,19 @@ if($mysqli->query($query)===TRUE){
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="../css/style.css" rel="stylesheet">
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition login-page bg">
 <div class="login-box">
   <div class="login-logo">
-    <a href="#"><b>Login</b>LTE</a>
+    <a href="#"><b class="black">PAK</b class="black">Animals</a>
+
   </div>
   <!-- /.login-logo -->
-  <div class="card">
+  <div class="card opacity bg-card">
     <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <p class="login-box-msg black">Sign in to start your session</p>
 
       <form  method="post">
         <div class="input-group mb-3">
@@ -84,31 +87,16 @@ if($mysqli->query($query)===TRUE){
         </div>
         <div class="row">
           <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="doctor" name="doctor" value="doctor">
-              <label for="doctor">
-                Sign in as Doctor
-              </label>
-            </div>
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" class="btn btn-outline-success btn-block">Sign In</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
 
-      <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-      <!-- /.social-auth-links -->
+ 
 
       <p class="mb-1">
         <a href="forgot-password.html">I forgot my password</a>
