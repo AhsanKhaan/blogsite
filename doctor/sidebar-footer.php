cac94976e2401f6b29fd524c7083c85fc27e6cd9
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-07 13:15:18 +0500

    change footer

--- doctor/sidebar-footer.php
+++ doctor/sidebar-footer.php
@@ -156,10 +156,10 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2020 <a href="http://www.bmsastech.com">BMSAS Technologies</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.5
+      <b>Version</b> 0.1
     </div>
   </footer>
 </div>
