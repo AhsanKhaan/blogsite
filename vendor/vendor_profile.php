0578ae35e2d82e78ae39eb8448a80f3e0ec180f5
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-07 23:28:56 +0500

    created user interface of cendor profile

--- vendor/vendor_profile.php
+++ vendor/vendor_profile.php
@@ -2,6 +2,223 @@
     include './sidebar-header.php';
     ?>
 
+          <div class="col-sm-6">
+            <h1>Profile</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">User Profile</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+
+            <!-- Profile Image -->
+            <div class="card card-primary card-outline">
+              <div class="card-body box-profile">
+                <div class="text-center">
+                  <img class="profile-user-img img-fluid img-circle"
+                       src="../dist/img/user4-128x128.jpg"
+                       alt="User profile picture">
+                </div>
+
+                <h3 class="profile-username text-center">Qasim</h3>
+
+                <p class="text-muted text-center">Bird whole Seller</p>
+
+                <ul class="list-group list-group-unbordered mb-3">
+                  <li class="list-group-item">
+                    <b>Total Products</b> <a class="float-right">1,322</a>
+                  </li>
+                  <li class="list-group-item">
+                    <b>Following</b> <a class="float-right">543</a>
+                  </li>
+
+                </ul>
+
+                <a href="#" class="btn btn-primary btn-block"><b>Contact Me</b></a>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <!-- About Me Box -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">About Me</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <!-- <strong><i class="fas fa-book mr-1"></i> Education</strong>
+
+                <p class="text-muted">
+                  M.B.B.S in Physiotheraphy
+                </p>
+
+                <hr> -->
+
+                <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+
+                <p class="text-muted">Sindh, karachi</p>
+
+                <!-- <hr>
+
+                <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+
+                <p class="text-muted">
+                  <span class="tag tag-danger">Consultant</span>
+                  <span class="tag tag-success">Physiotheraphy</span>
+                  <span class="tag tag-info">Vaccination</span>
+                </p> -->
+
+                <hr>
+
+                  <strong><i class="fa fa-address-card mr-1"></i> Contact</strong>
+
+                  <p class="text-muted">
+                    <span ><i class="fa fa-phone" aria-hidden="true"></i><b>0321-1234567</b></span>
+                    <br>
+                    <span ><i class="fa fa-envelope" aria-hidden="true"></i><b><a href="mailto:example@gmail.com">example@gmail.com</a></b></span>
+                    
+                  </p>
+
+                <hr>
+
+                <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+
+                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-9">
+            <div class="card">
+              <div class="card-header p-2">
+                <ul class="nav nav-pills">
+                  <li class="nav-item"><a class="nav-link active" href="#products" data-toggle="tab">Products</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Settings</a></li>
+                </ul>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content">
+                  <div class="active tab-pane" id="products">
+                  <table class="table table-bordered">
+                  <thead>                  
+                    <tr>
+                      <th style="width: 10px">#</th>
+                      <th style="width: 170px">Product Name</th>
+                      <th>Description</th>
+                      <th>Sale Price</th>
+                      <th style="width: 40px">Qty</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Wild Animals</td>
+                      <td>
+                       <p>Category: Lion</p>
+                      </td>
+                      <td>
+                         <p>$100</p>
+                      </td>
+                      <td><span class="badge ">1</span></td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>Pitbull</td>
+                      <td>
+                          <p>Category:Carnivourous</p>
+                      </td>
+                      <td>
+                         <p>$100</p>
+                      </td>
+                      <td><span class="badge ">10</span></td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>Persian Cat</td>
+                      <td>
+                        <p>Category:Carnivourous </p>
+                      </td>
+                      <td>
+                         <p>$100</p>
+                      </td>
+                      <td><span class="badge ">13</span></td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>black mamba</td>
+                      <td>
+                        <p>Category:reptiles</p>
+                      </td>
+                      <td>
+                         <p>$100</p>
+                      </td>
+                      <td><span class="badge ">9</span></td>
+                    </tr>
+                  </tbody>
+                </table>
+                  </div>
+                  <!-- /.tab-pane -->
+                
+
+
+                  <div class="tab-pane" id="settings">
+                    <form class="form-horizontal">
+                      <div class="form-group row">
+                        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputName" placeholder="Name">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                          <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                       <label for="inputExperience" class="col-sm-2 col-form-label">Description</label>
+                        <div class="col-sm-10">
+                          <textarea class="form-control" id="inputExperience" placeholder="Enter your Description"></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <div class="offset-sm-2 col-sm-10">
+                          <div class="checkbox">
+                            <label>
+                              <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <div class="offset-sm-2 col-sm-10">
+                          <button type="submit" class="btn btn-danger">Submit</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                  <!-- /.tab-pane -->
+                </div>
+                <!-- /.tab-content -->
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.nav-tabs-custom -->
+          </div>
+          <!-- /.col -->
+
     <?php 
     include './sidebar-footer.php';
     ?>
