6f8a63c7930143ad1ab6631e8f290b87643f5ee4
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-10 20:13:45 +0500

    remove demo project from master

--- js/script.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-    // function append_sidebar_items(){
-    //     var html="<li class='nav-item'><a href='pages/calendar.html' class='nav-link'><i class='nav-icon fas fa-calendar-alt'></i><p>Calendar<span class='badge badge-info right'>2</span></p></a></li>";
-    //     return $("#side_bar").append('"'+html+'"');
-    // }
-    });
