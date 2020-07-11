c510a09c054df2a7754fda607b57e248c84e0d5b
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-11 15:20:13 +0500

    added managed categories in project

--- admin/categories.php
+++ admin/categories.php
@@ -10,72 +10,8 @@
 ?>
     
    <div class="content-wrapper">
-<section class="content-header">
-  <div class="container-fluid">
-    <div class="row mb-2">
-      <div class="col-sm-6">
-        <h1>Categories</h1>
-      </div>
-      <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="<?php echo $SiteUrl; ?>admin/dashboard.php">Home</a></li>
-          <li class="breadcrumb-item active">Categories Form</li>
-        </ol>
-      </div>
-    </div>
-  </div><!-- /.container-fluid -->
-</section>
-
-<div class="table-responsive">
-  
-                                <table id="example-datatable" class="text-center table table-vcenter table-condensed table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center"><input type="checkbox" class="allcheckbox" value=""></th>
-                                            <th class="text-center">S.no</th>
-                                            <th class="text-center">Image</th>
-                                            <th class="text-center">Name</th>
-                                            <th class="text-center">Status</th>
-                                            <th class="text-center">Home Status</th>
-                                            <th class="text-center">Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php if (mysqli_num_rows($AllCategoriesQuery) > 0): $count = 1;?>
-                                            <?php while($Categories = mysqli_fetch_assoc($AllCategoriesQuery)): ?>
-                                                 <tr>
-                                                    <td class="text-center"><input type="checkbox" class="colcheckbox" name="colcheckbox[]" value="<?php echo $Categories['category_id']; ?>"></td>
-                                                    <td class="text-center"><?php echo $count;  ?></td>
-                                                    <td class="text-center"><a href="javascript:void(0)">
-<?php if ($Categories['category_imgsm'] == null || !file_exists('../' . $Categories['category_imgsm'])): ?>
-     <small><strong>No Image Uploaded</strong></small>
-    <?php else: ?>
-        <img src="<?php echo $SiteUrl . $Categories['category_imgsm']; ?>" alt="Category Image" style="height: 30px; object-fit: cover;">
-<?php endif ?>
-                                                    </a></td>
-                                                    <td class="text-center"><a href="javascript:void(0)"><?php echo $Categories['category_name'] ?></a></td>
-                                                    <td class="text-center"><label class="switch switch-success"><input type="checkbox" <?php echo $Categories['category_status'] == 1 ? 'checked' : null; ?>  name="cat_status" value="1" class="status" data-cat_id="<?php echo $Categories['category_id']; ?>"><span></span></label> </td>
-                                               
-                                                    <td class="text-center">
-                                                         <label class="switch switch-success"><input type="checkbox" name="cat_displayhome" value="1" <?php echo $Categories['category_displayhome'] == 1 ? 'checked' : null; ?> class="display_status" data-cat_id="<?php echo $Categories['category_id']; ?>"><span></span></label>
-                                                    </td>
-                                                    
-                                                    <td class="text-center">
-                                                        <div class="btn-group">
-                                                            <a href="<?php echo $SiteUrl ?>admin/manage_categories.php?catid=<?php echo $Categories['category_id']; ?>" data-toggle="tooltip" title="Edit" class="btn btn-xs btn-default"><i class="fa fa-pencil"></i></a>
-                                                            <a href="javascript:void(0)" data-toggle="tooltip" title="Delete" class="btn btn-xs btn-danger deletecategory" data-cat_id="<?php echo $Categories['category_id']; ?>" ><i class="fa fa-times"></i></a>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            <?php $count++; endwhile; ?>
-                                        <?php endif ?>
-                                    </tbody>
-                                </table>
-                            
-</div>
-
- </div>
-           
+   <h1>HELLO WORLD</h1>
+  </div>           
 
 
 <?php 
---
+++ admin/js/category.js
@@ -0,0 +1,197 @@
+$(function(){
+   var SendURL = $('#url').val();
+   function loadNotification (type, theme, message, time){
+      $.getScript( SendURL + "admin/assets/js/notification.js" )
+      .done(function(script, textStatus ) {
+        noty(type, theme, message, time);
+      })
+      .fail(function( jqxhr, settings, exception ) {
+      });
+    }
+
+    function ShowHideSpinner(time = ''){
+        var setTime = $.trim(time) == '' ? 1000 : time;
+      $('.spinner').css('display', 'inline-block');
+      setTimeout(function(){
+          $('.spinner').css('display', 'none');
+      }, setTime);
+    }
+
+    function SmoothScroll(element){
+	    $('html, body').animate({
+	        scrollTop: $(element).offset().top
+	    }, 200);
+    }
+
+
+	   $('#categoryform').submit(function(e){
+        e.preventDefault();
+        var cat_name = $.trim($('#cat_name').val());
+        if($.trim(cat_name) == ''){
+           $('.catname-group').addClass('animated ' + 'bounce').one('animationend oAnimationEnd webkitAnimationEnd mozAnimationEnd', function() {
+           $(this).removeClass('animated ' + 'bounce')});
+           $('.catname-group input').css('border-color', 'red');
+            ShowHideSpinner();
+            SmoothScroll('.catname-group');
+            return false;
+         }else{
+           $('.catname-group input').css('border-color', '#dbe1e8');
+            // for ( instance in CKEDITOR.instances ){
+            //    CKEDITOR.instances[instance].updateElement();
+            //  }
+            $.ajax({
+                url: SendURL + "admin/back/Category_Setting.php",
+                method:"POST",
+                data: new FormData($('#categoryform')[0]),
+                contentType:false,
+                cache:false,
+                processData:false,
+                beforeSend:function(){
+                  $('.spinner').css('display', 'inline-block');
+                },
+                success:function(data){
+               
+
+                  $('.spinner').css('display', 'none');
+                  if($.trim(data) != 'success'){
+                    loadNotification ('error', 'metroui', data , 3000);
+                    return false;
+                  }else{
+                     window.location = SendURL + 'admin/categories.php';
+                    return false;
+                  }
+                }
+            })
+        }
+    });
+
+     function SendStatus(value, action, cat_id){
+         $.ajax({
+           url:SendURL + "admin/back/Category_Setting.php",
+           method:"POST",
+           data:{value:value, action:action, cat_id:cat_id},
+           success:function(data){
+              loadNotification ('success', 'metroui', "Status Has Been Changed Successfully" , 3000);
+           }
+         });
+     }
+     $(document).on('change', '.status', function(){
+       var value = $(this).is(":checked") ? 1 : 0;
+       var cat_id = $(this).data("cat_id");
+       SendStatus(value, 'Status', cat_id);
+     });  
+    
+     $(document).on('change', '.display_status', function(){
+       var value = $(this).is(":checked") ? 1 : 0;
+       var cat_id = $(this).data('cat_id');
+       SendStatus(value, 'HomeStatus', cat_id);
+     });  
+
+     $('.allcheckbox').click(function(){
+       if($(this).is(':checked')){
+         $('.colcheckbox').prop('checked', true);
+         $('.deleteall').fadeIn();
+       }else{
+         $('.colcheckbox').prop('checked', false);
+         $('.deleteall').fadeOut();
+       }
+     });
+
+     $('.colcheckbox').click(function(){
+       if($('.colcheckbox').is(':checked')){
+         $('.deleteall').fadeIn();
+       }else{
+         $('.deleteall').fadeOut();
+       }
+     });
+
+
+     $(document).on('click', '.deleteall', function(){
+       var length = $('[name="colcheckbox[]"]:checked').length;
+       if(length > 0){
+         var CheckedValue = [];
+         $('[name="colcheckbox[]"]:checked').each(function(i){
+           CheckedValue.push($(this).val());
+         });
+
+          Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.value) {
+               $.ajax({
+                  url:SendURL + "admin/back/Category_Setting.php",
+                  method:"POST",
+                  data:{catids:CheckedValue, action:"DeleteBulkRecord"},
+                  success:function(data){
+                    if($.trim(data) != 'success'){
+                       loadNotification ('error', 'metroui', data , 3000);
+                       setTimeout(function(){
+                          location.reload();
+                        }, 3500);
+                       return false;
+                    }else{
+                      if (result.value) {
+                        Swal.fire(
+                          'Deleted!',
+                          'Record Has Been Deleted.',
+                          'success'
+                        )
+                      }
+                    }
+                    setTimeout(function(){
+                      location.reload();
+                    }, 3500);
+
+                  }
+                });
+            }
+          });
+       }else{
+         loadNotification ('error', 'metroui', "Please Select Atleast One Record" , 3000);
+         return false;
+       }
+     });  
+
+     $(document).on('click', '.deletecategory', function(){
+       var value = $(this).data('cat_id');
+       var element = $(this);
+        Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.value) {
+          $.ajax({
+            url:SendURL + "admin/back/Category_Setting.php",
+            method:"POST",
+            data:{catid:value, action:"DeleteSingleRecord"},
+            success:function(data){
+              if($.trim(data) != 'success'){
+                loadNotification ('error', 'metroui', data , 3000);
+                return false;
+              }else{
+                if (result.value) {
+                  Swal.fire(
+                    'Deleted!',
+                    'Record Has Been Deleted.',
+                    'success'
+                  )
+                }
+                element.parent().parent().parent().slideUp();
+              }
+            }
+          });
+        }
+      });
+     });  
+})
