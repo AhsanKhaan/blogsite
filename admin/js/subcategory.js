ee25c072ddade59d9728ffb98f4c0e9d67db48b0
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-12 21:52:14 +0500

    added subcategory coding

---
+++ admin/js/subcategory.js
@@ -0,0 +1,217 @@
+$(function(){
+   var SendURL = $('#url').val();
+   function loadNotification (type, theme, message, time){
+      $.getScript( SendURL + "admin/assets/js/notification.js" )
+      .done(function(script, textStatus ) {
+        noty(type, theme, message, time);
+      })
+      .fail(function( jqxhr, settings, exception ) {
+      });
+    }
+
+    function ShowHideSpinner(time = ''){
+      var setTime = $.trim(time) == '' ? 1000 : time;
+      $('#spinner').css('display', 'inline-block');
+      setTimeout(function(){
+          $('#spinner').css('display', 'none');
+      }, setTime);
+    }
+
+    function SmoothScroll(element){
+	    $('html, body').animate({
+	        scrollTop: $(element).offset().top
+	    }, 200);
+    }
+
+
+	   $('#subcategoryform').submit(function(e){
+        e.preventDefault();
+        var cat_name = $.trim($('.categorySelect').val());
+        var subcat_name = $.trim($('#subcat_name').val());
+        if($.trim(cat_name) == ''){
+           $('.category-group').addClass('animated ' + 'bounce').one('animationend oAnimationEnd webkitAnimationEnd mozAnimationEnd', function() {
+           $(this).removeClass('animated ' + 'bounce')});
+           $('.category-group select, .category-group a').css('border', '1px solid red');
+            ShowHideSpinner();
+            SmoothScroll('.category-group');
+            return false;
+         }else if ($.trim(subcat_name) == ''){
+           $('.category-group select, .category-group a').css('border', '1px solid #dbe1e8');
+           $('.subcatname-group').addClass('animated ' + 'bounce').one('animationend oAnimationEnd webkitAnimationEnd mozAnimationEnd', function() {
+           $(this).removeClass('animated ' + 'bounce')});
+           $('.subcatname-group input').css('border-color', 'red');
+           ShowHideSpinner();
+           SmoothScroll('.subcatname-group');
+           return false;
+         }else{
+           $('.subcatname-group input').css('border-color', '#dbe1e8');
+            for ( instance in CKEDITOR.instances ){
+               CKEDITOR.instances[instance].updateElement();
+             }
+            $.ajax({
+                url: SendURL + "admin/back/SubCategory_Setting.php",
+                method:"POST",
+                data: new FormData($('#subcategoryform')[0]),
+                contentType:false,
+                cache:false,
+                processData:false,
+                beforeSend:function(){
+                  $('#spinner').css('display', 'inline-block');
+                },
+                success:function(data){
+                  $('#spinner').css('display', 'none');
+                  if($.trim(data) != 'success'){
+                    loadNotification ('error', 'metroui', data , 3000);
+                    return false;
+                  }else{
+                    window.location = SendURL + 'admin/subcategories.php';
+                    return false;
+                  }
+                }
+            })
+        }
+    });
+
+     function SendStatus(value, action, subcat_id){
+         $.ajax({
+           url:SendURL + "admin/back/SubCategory_Setting.php",
+           method:"POST",
+           data:{value:value, action:action, subcat_id:subcat_id},
+           success:function(data){
+              loadNotification ('success', 'metroui', "Status Has Been Changed Successfully" , 3000);
+           }
+         });
+     }
+
+     $(document).on('change', '.status', function(){
+       var value = $(this).is(":checked") ? 1 : 0;
+       var subcat_id = $(this).data('subcat_id');
+       SendStatus(value, 'Status', subcat_id);
+     });  
+    
+
+     $(document).on('change', '.categorySelect', function(){
+       var CategoryValue = $.trim($(this).val());
+       if(CategoryValue != ''){
+         $('.subcategorybtn').prop('disabled', false);
+       }else{
+         $('.subcategorybtn').prop('disabled', true);
+       }
+    });  
+
+     var CategoryValue = $.trim($('.categorySelect').val());
+     if(CategoryValue != ''){
+       $('.subcategorybtn').prop('disabled', false);
+     }else{
+       $('.subcategorybtn').prop('disabled', true);
+     }
+
+     $('.allcheckbox').click(function(){
+       if($(this).is(':checked')){
+         $('.colcheckbox').prop('checked', true);
+         $('.deleteall').fadeIn();
+       }else{
+         $('.colcheckbox').prop('checked', false);
+         $('.deleteall').fadeOut();
+       }
+     });
+
+
+     $('.colcheckbox').click(function(){
+       if($('.colcheckbox').is(':checked')){
+         $('.deleteall').fadeIn();
+       }else{
+         $('.deleteall').fadeOut();
+       }
+     });
+       
+       $(document).on('click', '.deleteall', function(){
+       var length = $('[name="colcheckbox[]"]:checked').length;
+       if(length > 0){
+         var CheckedValue = [];
+         $('[name="colcheckbox[]"]:checked').each(function(i){
+           CheckedValue.push($(this).val());
+         });
+
+          Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+             if (result.value) {
+                $.ajax({
+                  url:SendURL + "admin/back/SubCategory_Setting.php",
+                  method:"POST",
+                  data:{subcatids:CheckedValue, action:"DeleteBulkRecord"},
+                  success:function(data){
+                    if($.trim(data) != 'success'){
+                       loadNotification ('error', 'metroui', data , 3000);
+                       setTimeout(function(){
+                          location.reload();
+                        }, 3500);
+                       return false;
+                    }else{
+                      if (result.value) {
+                        Swal.fire(
+                          'Deleted!',
+                          'Record Has Been Deleted.',
+                          'success'
+                        )
+                      }
+                    }
+                    setTimeout(function(){
+                      location.reload();
+                    }, 3500);
+
+                  }
+                });
+             }
+          });
+       }else{
+         loadNotification ('error', 'metroui', "Please Select Atleast One Record" , 3000);
+         return false;
+       }
+     });  
+
+
+     $(document).on('click', '.deletesubcategory', function(){
+       var value = $(this).data('subcat_id');
+       var element = $(this);
+        Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.value) {
+           $.ajax({
+            url:SendURL + "admin/back/SubCategory_Setting.php",
+            method:"POST",
+            data:{subcatid:value, action:"DeleteSingleRecord"},
+            success:function(data){
+              if($.trim(data) != 'success'){
+                loadNotification ('error', 'metroui', data , 3000);
+                return false;
+              }else{
+                if (result.value) {
+                  Swal.fire(
+                    'Deleted!',
+                    'Record Has Been Deleted.',
+                    'success'
+                  )
+                }
+                element.parent().parent().parent().slideUp();
+              }
+            }
+          });
+        }
+      });
+     });  
+})
---
+++ admin/manage_subcategories.php
@@ -0,0 +1,379 @@
+<?php 
+  include('inc/config.php');
+  include('inc/functions.php');
+  include 'inc/Authorize.php'; 
+  $PageTitle = ' Manage Sub Categories | ';
+  include('header.php');
+  include('topbar.php');
+  include('sidebar.php');
+  
+?>
+<div class="content-wrapper">
+    <!-- Page content -->
+<div id="page-content">
+    <div class="content-header">
+        <div class="header-section">
+            <?php if (isset($_GET['subcatid'])): ?>
+                <?php 
+                    $subcatid = preg_replace('/[^0-9]/', '-', $_GET['subcatid']);
+                    $SubcategeryData = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM subcategory WHERE subcategory_id = ".$subcatid.""));
+                    if(!isset($SubcategeryData)){
+                        echo '<script> window.location = "'.$SiteUrl.'admin/subcategories.php"; </script>';
+                        exit();
+                    }
+                 ?>
+                <h1>
+                      <strong><?php echo $SubcategeryData['subcategory_name']; ?></strong> Subcategory
+                </h1>
+                <?php else: ?>
+                <h1>
+                      <strong>Add New</strong> Subcategory
+                </h1>
+            <?php endif ?>
+        </div>
+    </div>
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Manage Categories</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="<?php echo $SiteUrl; ?>admin/dashboard.php">dashboard</a></li>
+              <li class="breadcrumb-item active">Categories Form</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <ul class="breadcrumb breadcrumb-top">
+        <li>Sub Categories</li>
+        <li><a href="">Manage Sub Categories</a></li>
+    </ul>
+<div class="col-md-12">   
+            <!-- general form elements -->
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Manage Sub-Categories Settings</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form id="subcategoryform" enctype="multipart/form-data" class="form-horizontal form-bordered">
+                <div class="card-body">
+                <div class="form-group category-group">
+                        <label class="col-md-3 control-label" for="cat_image">Select Category</label>
+                        <div class="col-md-9">
+                        <?php 
+                            $CategoryQuery = mysqli_query($conn, "SELECT * FROM category WHERE category_status = 1");
+                        ?>    
+                        <select name="category-chosen" class="categorySelect select-chosen" data-placeholder="Select Category.." style="width: 250px;">
+                            <?php if (mysqli_num_rows($CategoryQuery) > 0): ?>
+                                <option value=""></option>
+                                <?php while($Category = mysqli_fetch_assoc($CategoryQuery)): ?>
+                                    <option <?php echo isset($SubcategeryData['category_id']) && $SubcategeryData['category_id'] == $Category['category_id'] ? 'selected' : null; ?> value="<?php echo $Category['category_id']; ?>"><?php echo $Category['category_name']; ?></option>
+                                <?php endwhile; ?>
+                                
+                                <?php else: ?>
+                                <option value=""></option>
+                                <option value="">No Categories Are Active</option>
+                            <?php endif ?>
+                        </select>
+                         
+                        </div>
+                    </div>
+
+
+<div class="row">
+	<div class="col-md-3">
+		<div class="form-group name-group">
+			<label class="control-label" for="subcat_image">SubCategory Image</label>
+			<div class="col-md-3">
+				<input type="file" id="subcat_image" name="subcat_image" value="">
+			</div>
+		</div>
+	</div>
+	
+
+	<div class="col-md-6 subcatname-group">
+		<div class="form-group subcatname-group">
+			<label class="control-label" for="subcat_name">SubCategory Name</label>
+				<input type="text" <?php echo TypeLength(); ?> id="subcat_name" name="subcat_name" class="form-control" placeholder="SubCategory Name" value="<?php echo $CategeryData['category_name'] ?? null; ?>">
+		</div>
+	</div>
+	<div class="col-md-3">
+		<div class="form-group name-group">
+			<label class="" for="subcat_price">SubCategory Price <small style="color:red;"><strong>(Percentage)</strong></small></label>
+			<input type="text" id="subcat_price" name="subcat_price" class="form-control" placeholder="SubCategory Price" value="<?php echo $CategeryData['price'] ?? null; ?>">
+		</div>
+	</div>
+
+<div class="col-md-6">
+    <div class="form-group name-group">
+        <label class=" control-label" for="subcat_slug">SubCategory Slug</label>
+            <input type="text" <?php echo TypeLength(); ?> id="subcat_slug" name="subcat_slug" class="form-control" placeholder="SubCategory Slug" value="<?php echo $CategeryData['category_slug'] ?? null; ?>">
+    </div>
+</div>
+
+<!-- <div class="form-group name-group">
+                        <label class="col-md-3 control-label" for="subcat_salecaption">Sale Caption</label>
+                        <div class="col-md-3">
+                            <input type="text" id="subcat_salecaption" name="subcat_salecaption" class="form-control" placeholder="Sale Caption" <?php echo TypeLength(); ?> value="<?php echo $SubcategeryData['subcategory_salecaption'] ?? null; ?>">
+                        </div>
+                         <label class="col-md-3 control-label" for="subcat_sortorder">Sort Order</label>
+                        <div class="col-md-3">
+                            <input type="text" id="subcat_sortorder" name="subcat_sortorder" class="form-control" placeholder="Sort Order" value="<?php echo $SubcategeryData['subcategory_sort'] ?? null; ?>">
+                        </div>
+                    </div> -->
+<div class="col-md-6">
+     <div class="form-group name-group">
+        <label class="control-label" for="subcat_salecaption">Sale Caption</label>
+            <input type="text" <?php echo TypeLength(); ?> id="subcat_salecaption" name="subcat_salecaption" class="form-control" placeholder="Sale Caption" value="<?php echo $CategeryData['category_salecaption'] ?? null; ?>">
+    </div>
+</div>
+
+<div class="col-md-6">
+        
+         <label class="control-label" for="subcat_sortorder">Sort Order</label>
+        
+            <input type="text" id="subcat_sortorder" name="subcat_sortorder" class="form-control" placeholder="Sort Order" value="<?php echo $CategeryData['category_sort'] ?? null; ?>">
+        
+    </div>
+
+
+<div class="col-md-3">
+	<div class="form-group">
+		<label for=""></label>
+		<div class="custom-control custom-checkbox">
+          <input class="custom-control-input" type="checkbox" <?php echo isset($CategeryData['category_status']) &&  $CategeryData['category_status'] == 1 ? 'checked' : null; ?>  name="cat_status" value="1">
+          <label for="customCheckbox2" class="custom-control-label">Category Status</label>
+        </div>
+		  
+                        
+	</div>
+</div>
+
+
+<div class="col-md-3">
+	<div class="form-group">
+		<label for=""></label>
+		<div class="custom-control custom-checkbox">
+          <input class="custom-control-input" type="checkbox" name="cat_displayhome" value="1" <?php echo isset($CategeryData['category_displayhome']) &&  $CategeryData['category_displayhome'] == 1 ? 'checked' : null; ?>>
+          <label for="customCheckbox1" class="custom-control-label">Display On Home</label>
+        </div>               
+	</div>
+</div>
+
+<div class="col-md-12">
+	<div class="form-group">
+	 <label class="control-label" for="cat_shortdesc">Short Description</label>
+  
+      <textarea id="cat_shortdesc" name="cat_shortdesc" rows="3" class="form-control" placeholder="Short Description" <?php echo TypeLength(490); ?>><?php echo $CategeryData['category_shortdesc'] ?? null; ?></textarea>
+    </div>
+</div>
+
+
+<div class="col-md-12">
+	<div class="form-group">
+  <label class="control-label" for="cat_longdesc">Long Description</label>
+      
+    <textarea id="textarea-ckeditor" rows="7" name="cat_longdesc" class="textarea form-control" placeholder="Long Description"><?php echo isset($CategeryData['category_longdesc']) ? stripslashes($CategeryData['category_longdesc']) : null; ?></textarea>
+        </div>
+    
+</div>
+
+
+
+<div class="col-md-12">
+	  <div class="form-group name-group">
+            <label class="control-label" for="metatitle">Meta Title</label>
+                <input type="text" <?php echo TypeLength(); ?> id="metatitle" name="metatitle" class="form-control" placeholder="Meta Title" value="<?php echo $CategeryData['title'] ?? null; ?>">
+        </div>
+	</div>
+
+	<div class="col-md-12">
+		   <div class="form-group name-group">
+                        <label class="control-label" for="metadesc">Meta Description</label>
+                        
+                          <textarea <?php echo TypeLength(); ?> id="metadesc" name="metadesc" rows="3" class="form-control" placeholder="Meta Description"><?php echo $CategeryData['description'] ?? null; ?></textarea>
+                        
+                    </div>
+	</div>
+
+	<div class="col-md-12">
+		  <div class="form-group name-group">
+                        <label class="control-label" for="metakeyword">Meta Keyword</label>
+                        
+                          <textarea <?php echo TypeLength(); ?> id="metakeyword" name="metakeyword" rows="3" class="form-control" placeholder="Meta Keyword"><?php echo $CategeryData['keyword'] ?? null; ?></textarea>
+                        
+                    </div>
+	</div>
+</div>
+                   
+
+
+                   
+                     
+
+                   
+ 
+               <input type="hidden" name="cat_id" id="cat_id" value="<?php echo $CategeryData['category_id'] ?? null; ?>">
+                    <div class="form-group form-actions">
+                        <div class="col-md-9">
+<button type="submit" class="btn btn-primary">SUBMIT 
+              &nbsp; <svg style="display: none;" class="spinner" width="30" height="30" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+   <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+</svg>
+</button>
+                        </div>
+                    </div>
+                
+                </div>
+             
+              </form>
+            </div>
+            <!-- /.card -->
+
+
+          </div>
+
+        
+    <div class="clearfix"></div>
+
+    
+</div>
+ 
+        <div class="col-md-12">
+            <!-- Basic Form Elements Block -->
+            <div class="block">
+                <!-- Basic Form Elements Title -->
+                <div class="block-title">
+                    <h2><strong>Sub Category </strong> Settings</h2>
+                </div>
+                <!-- END Form Elements Title -->
+
+                <!-- Basic Form Elements Content -->
+                <form id="subcategoryform" enctype="multipart/form-data" class="form-horizontal form-bordered">
+                    <div class="form-group category-group">
+                        <label class="col-md-3 control-label" for="cat_image">Select Category</label>
+                        <div class="col-md-9">
+                        <?php 
+                            $CategoryQuery = mysqli_query($conn, "SELECT * FROM category WHERE category_status = 1");
+                        ?>    
+                        <select name="category-chosen" class="categorySelect select-chosen" data-placeholder="Select Category.." style="width: 250px;">
+                            <?php if (mysqli_num_rows($CategoryQuery) > 0): ?>
+                                <option value=""></option>
+                                <?php while($Category = mysqli_fetch_assoc($CategoryQuery)): ?>
+                                    <option <?php echo isset($SubcategeryData['category_id']) && $SubcategeryData['category_id'] == $Category['category_id'] ? 'selected' : null; ?> value="<?php echo $Category['category_id']; ?>"><?php echo $Category['category_name']; ?></option>
+                                <?php endwhile; ?>
+                                
+                                <?php else: ?>
+                                <option value=""></option>
+                                <option value="">No Categories Are Active</option>
+                            <?php endif ?>
+                        </select>
+                         
+                        </div>
+                    </div>
+
+
+
+                    <div class="form-group subcatname-group">
+                        <label class="col-md-3 control-label" for="subcat_name">Subcategory Name</label>
+                        <div class="col-md-9">
+                            <input type="text" id="subcat_name" name="subcat_name" class="form-control" placeholder="Subcategory Name" <?php echo TypeLength(); ?> value="<?php echo $SubcategeryData['subcategory_name'] ?? null; ?>">
+                        </div>
+                    </div>
+
+                    <div class="form-group name-group">
+                        <label class="col-md-3 control-label" for="subcat_slug">Subcategory Slug</label>
+                        <div class="col-md-9">
+                            <input type="text" <?php echo TypeLength(); ?> id="subcat_slug" name="subcat_slug" class="form-control" placeholder="Subcategory Slug" value="<?php echo $SubcategeryData['subcategory_slug'] ?? null; ?>">
+                        </div>
+                    </div>
+
+                     <div class="form-group name-group">
+                        <label class="col-md-3 control-label" for="subcat_salecaption">Sale Caption</label>
+                        <div class="col-md-3">
+                            <input type="text" id="subcat_salecaption" name="subcat_salecaption" class="form-control" placeholder="Sale Caption" <?php echo TypeLength(); ?> value="<?php echo $SubcategeryData['subcategory_salecaption'] ?? null; ?>">
+                        </div>
+                         <label class="col-md-3 control-label" for="subcat_sortorder">Sort Order</label>
+                        <div class="col-md-3">
+                            <input type="text" id="subcat_sortorder" name="subcat_sortorder" class="form-control" placeholder="Sort Order" value="<?php echo $SubcategeryData['subcategory_sort'] ?? null; ?>">
+                        </div>
+                    </div>
+ 
+
+                     <div class="form-group name-group">
+                        <label class="col-md-3 control-label">Subcategory Status</label>
+                        <div class="col-md-3">
+                              <label class="switch switch-success"><input type="checkbox" <?php echo isset($SubcategeryData['subcategory_status']) &&  $SubcategeryData['subcategory_status'] == 1 ? 'checked' : null; ?>  name="subcat_status" value="1"><span></span></label>
+                        </div>
+                      </div>
+                    <div class="form-group name-group">
+                      <label class="col-md-3 control-label" for="subcat_image">Subcategory Image</label>
+                      <div class="col-md-3">
+                        <input type="file" id="subcat_image" name="subcat_image" value="">
+                     </div>
+                    </div>
+
+
+                     <div class="form-group name-group">
+                        <label class="col-md-3 control-label" for="subcat_shortdesc">Short Description</label>
+                        <div class="col-md-9">
+                          <textarea id="subcat_shortdesc" name="subcat_shortdesc" rows="3" class="form-control" placeholder="Short Description" <?php echo TypeLength(495); ?>><?php echo $SubcategeryData['subcategory_shortdesc'] ?? null; ?></textarea>
+                        </div>
+                    </div>
+
+                     <div class="form-group name-group">
+                        <label class="col-md-3 control-label" for="subcat_longdesc">Long Description</label>
+                        <div class="col-md-9">
+                            <textarea id="textarea-ckeditor" rows="7" name="subcat_longdesc" class="ckeditor form-control" placeholder="Long Description"><?php echo isset($SubcategeryData['subcategory_longdesc']) ? stripslashes($SubcategeryData['subcategory_longdesc']) : null; ?></textarea>
+                        </div>
+                    </div>
+
+                     <div class="form-group name-group">
+                        <label class="col-md-3 control-label" for="metatitle">Meta Title</label>
+                        <div class="col-md-9">
+                            <input type="text" <?php echo TypeLength(); ?> id="metatitle" name="metatitle" class="form-control" placeholder="Meta Title" value="<?php echo $SubcategeryData['title'] ?? null; ?>">
+                        </div>
+                    </div>
+
+                    <div class="form-group name-group">
+                        <label class="col-md-3 control-label" for="metadesc">Meta Description</label>
+                        <div class="col-md-9">
+                          <textarea <?php echo TypeLength(); ?> id="metadesc" name="metadesc" rows="3" class="form-control" placeholder="Meta Description"><?php echo $SubcategeryData['description'] ?? null; ?></textarea>
+                        </div>
+                    </div>
+
+                    <div class="form-group name-group">
+                        <label class="col-md-3 control-label" for="metakeyword">Meta Keyword</label>
+                        <div class="col-md-9">
+                          <textarea <?php echo TypeLength(); ?> id="metakeyword" name="metakeyword" rows="3" class="form-control" placeholder="Meta Keyword"><?php echo $SubcategeryData['keyword'] ?? null; ?></textarea>
+                        </div>
+                    </div>
+                    
+               <input type="hidden" name="csrf" id="csrf" value="<?php echo GenerateCSRF();  ?>">
+               <input type="hidden" name="subcat_id" id="subcat_id" value="<?php echo $SubcategeryData['subcategory_id'] ?? null; ?>">
+                    <div class="form-group form-actions">
+                        <div class="col-md-9 col-md-offset-3">
+                          <?php $ButtonText = 'Submit'; ?>
+                          <?php $DisabledSubmitBTN = TRUE; ?>
+                          <?php $SubmitBTNClass = 'subcategorybtn '; ?>
+                          <?php include('inc/spinnerbutton.php'); ?>
+                        </div>
+                    </div>
+                </form>
+                <!-- END Basic Form Elements Content -->
+            </div>
+            <!-- END Basic Form Elements Block -->
+        </div>
+    <div class="clearfix"></div>
+</div>
+<!-- END Page Content -->
+</div>
+<?php 
+  include('bottom.php');
+  include('footer.php');
+?>
+<script src="<?php echo $SiteUrl; ?>admin/js/subcategory.js"></script>
