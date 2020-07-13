4d858b5e472a26f572a17e48907a507acc335584
AhsanKhaan <ahsankhan.ubit@gmail.com>
2020-07-13 14:41:17 +0500

    adedd vendor classes

---
+++ admin/back/Vendor_Setting.php
@@ -0,0 +1,146 @@
+<?php 
+	require_once('../inc/config.php');
+	require_once('../inc/functions.php');
+	require_once('../inc/resize.image.class.php');
+
+	
+
+	if($_SERVER['REQUEST_METHOD'] == 'POST' AND isset($_POST) AND isset($_POST['action']) AND $_POST['action'] != 'DeleteSingleRecord' && $_POST['action'] != 'DeleteBulkRecord'){
+		if($_POST['action'] == 'Status'){
+			// Change Status
+			$cat_id = mysqli_real_escape_string($conn, CleanString($_POST['cat_id']));
+			$value = mysqli_real_escape_string($conn, CleanString($_POST['value']));
+			mysqli_query($conn, "UPDATE category SET 
+		 	`category_status` = ".$value."
+		 	 WHERE category_id = ".$cat_id."");
+		}else{
+			// Change Display Home Status
+			$cat_id = mysqli_real_escape_string($conn, CleanString($_POST['cat_id']));
+			$value = mysqli_real_escape_string($conn, CleanString($_POST['value']));
+			mysqli_query($conn, "UPDATE category SET 
+		 	`category_displayhome` = ".$value."
+		 	 WHERE category_id = ".$cat_id."");
+		}
+		exit('success');
+	}
+
+	if($_SERVER['REQUEST_METHOD'] == 'POST' AND isset($_POST) AND isset($_POST['action']) && $_POST['action'] == 'DeleteSingleRecord'){
+		 $cat_id = mysqli_real_escape_string($conn, CleanString($_POST['catid']));
+		 $SubCatCount = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM subcategory WHERE category_id = ".$cat_id.""));
+		 // $ProductsCatCount = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM product WHERE category_id = ".$cat_id.""));
+		 
+		 if($SubCatCount > 0){
+		 	exit("Please Delete The Assign Subcategory First");
+		 }
+
+		 // if($ProductsCatCount > 0){
+		 // 	exit("Please Delete The Assign Product First");
+		 // }
+
+		 mysqli_query($conn, "UPDATE category SET `del_status` = 1 WHERE category_id = ".$cat_id."");
+		 exit('success');
+	}
+
+	if($_SERVER['REQUEST_METHOD'] == 'POST' AND isset($_POST) AND isset($_POST['action']) && $_POST['action'] == 'DeleteBulkRecord'){
+		 $Count = null;
+		 foreach($_POST['catids'] as $catid){
+		 	 $SubCatCount = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM subcategory WHERE category_id = ".$catid.""));
+		 	 if($SubCatCount > 0){
+			 	$Count = 'set';
+			 	$msg = "Please Delete The Assign Subcategory First";
+			 	continue;
+			 }
+			 // $ProductsCatCount = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM product WHERE category_id = ".$cat_id.""));
+			 // if($ProductsCatCount > 0){
+			 // 	$Count = 'set';
+			 // $msg = "Please Delete The Assign Product First";
+			 // continue;
+			 // }
+		 	mysqli_query($conn, "UPDATE category SET `del_status` = 1 WHERE category_id = ".$catid."");
+		 }
+		 $Count != 'set' ? exit('success') : exit($msg);
+	}	
+
+
+	if($_SERVER['REQUEST_METHOD'] == 'POST' AND isset($_POST)){
+		sleep(1);
+		$cat_name = mysqli_real_escape_string($conn, CleanString($_POST['cat_name']));
+		$cat_slug = GenerateSlug(mysqli_real_escape_string($conn, CleanString($_POST['cat_slug'])), $cat_name);
+		$cat_salecaption = mysqli_real_escape_string($conn, CleanString($_POST['cat_salecaption']));
+		$cat_status = isset($_POST['cat_status']) ? mysqli_real_escape_string($conn, CleanString($_POST['cat_status'])) : 0;
+		$cat_displayhome = isset($_POST['cat_displayhome']) ? mysqli_real_escape_string($conn, CleanString($_POST['cat_displayhome'])) : 0;
+
+		$cat_price = !empty($_POST['cat_price']) ? mysqli_real_escape_string($conn, CleanString($_POST['cat_price'])) : 0;
+		
+		$cat_sortorder = !empty(trim($_POST['cat_sortorder'])) ? mysqli_real_escape_string($conn, CleanString($_POST['cat_sortorder'])) : 0 ;
+		$cat_shortdesc = mysqli_real_escape_string($conn, CleanString($_POST['cat_shortdesc']));
+		$cat_longdesc = mysqli_real_escape_string($conn, addslashes($_POST['cat_longdesc']));
+		$metatitle = SetEmptyFields(mysqli_real_escape_string($conn, CleanString($_POST['metatitle'])), $cat_name);
+		$metadesc = SetEmptyFields(mysqli_real_escape_string($conn, CleanString($_POST['metadesc'])), $cat_name);
+		$metakeyword = SetEmptyFields(mysqli_real_escape_string($conn, CleanString($_POST['metakeyword'])), $cat_name);
+		$cat_id = isset($_POST['cat_id']) ? mysqli_real_escape_string($conn, CleanString($_POST['cat_id'])) : null;
+		
+		
+		$CategeryData = null;
+		if(!empty($cat_name)){
+				if(isset($cat_id) && !empty($cat_id)){
+					// Update Record
+					 mysqli_query($conn, "UPDATE vendor SET 
+					 	`name` = '".$vendor_name."',
+					 	`category_slug` = '".$cat_slug."',
+					 	`category_shortdesc` = '".$cat_shortdesc."',
+					 	`category_longdesc` = '".$cat_longdesc."',
+					 	`category_sort` = ".$cat_sortorder.",
+					 	`price` = ".$cat_price.",
+					 	`category_status` = ".$cat_status.",
+					 	`category_displayhome` = ".$cat_displayhome.",
+					 	`category_salecaption` = '".$cat_salecaption."',
+					 	`title` = '".$metatitle."',
+					 	`keyword` = '".$metakeyword."',
+					 	`description` = '".$metadesc."'
+					 	 WHERE category_id = ".$cat_id."");
+					 	$Catgory_ID = $cat_id;
+			 		 $CategeryData = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM category WHERE category_id = ".$Catgory_ID.""));
+				}else{
+					// Insert Record
+					 mysqli_query($conn, "INSERT INTO category SET 
+					 	`category_name` = '".$cat_name."',
+					 	`category_slug` = '".$cat_slug."',
+					 	`category_shortdesc` = '".$cat_shortdesc."',
+					 	`category_longdesc` = '".$cat_longdesc."',
+					 	`category_sort` = ".$cat_sortorder.",
+					 	`price` = ".$cat_price.",
+					 	`category_status` = ".$cat_status.",
+					 	`category_displayhome` = ".$cat_displayhome.",
+					 	`category_salecaption` = '".$cat_salecaption."',
+					 	`title` = '".$metatitle."',
+					 	`keyword` = '".$metakeyword."',
+					 	`description` = '".$metadesc."'");
+					 $Catgory_ID = mysqli_insert_id($conn);
+				}
+
+			if(isset($_FILES['cat_image']['name']) && !empty($_FILES['cat_image']['name'])){
+				$name = basename($_FILES['cat_image']['name']);
+         		$extention = pathinfo($name, PATHINFO_EXTENSION);
+				$newname = bin2hex(random_bytes(5));
+         		$size = $_FILES['cat_image']['size'];
+         		$uploaddir = '../../upload/category/';
+         		if($size > (2048*2048)){
+         			exit("File Upload Size Limit Is 2 MB Only");
+         		}elseif($extention == "png" || $extention == "jpg" || $extention == "jpeg"){
+         			
+         			UploadFile($_FILES['cat_image']['tmp_name'], 1000, 500, 500, 500, TRUE, $newname, $uploaddir, $Catgory_ID, '../../' . $CategeryData['category_imglg'], '../../' . $CategeryData['category_imgsm'], 'category', 'category_imglg', 'category_imgsm', 'category_id', $Catgory_ID, $conn, $extention);
+         			
+         		}else{
+         			exit("Please Select The Images Only...!");
+         		} 
+			} 
+			exit('success');
+		}else{
+			exit('Please Enter The Category Name');
+		}
+
+	}
+
+	header('Location: ' . $SiteUrl . 'admin/index.php');
+	exit();
---
+++ admin/manage_vendor.php
@@ -0,0 +1,200 @@
+<?php 
+  include('inc/config.php');
+  include('inc/functions.php');
+  include 'inc/Authorize.php'; 
+  $PageTitle = ' Manage Categories | ';
+  include('header.php');
+  include('topbar.php');
+  include('sidebar.php');
+  
+?>
+<?php if (isset($_GET['catid'])): ?>
+    <?php 
+        $catid = preg_replace('/[^0-9]/', '-', $_GET['catid']);
+        $CategeryData = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM category WHERE category_id = ".$catid.""));
+        if(!isset($CategeryData)){
+            echo '<script> window.location = "'.$SiteUrl.'admin/categories.php"; </script>';
+            exit();
+        }
+     ?>
+<?php endif ?> 
+<div class="content-wrapper">
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Manage Categories</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="<?php echo $SiteUrl; ?>admin/dashboard.php">Home</a></li>
+          <li class="breadcrumb-item active">Categories Form</li>
+        </ol>
+      </div>
+    </div>
+  </div><!-- /.container-fluid -->
+</section>
+ 
+<div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Manage Catetegories Settings</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form id="vendorform" enctype="multipart/form-data" class="form-horizontal form-bordered">
+                <div class="card-body">
+<div class="row">
+	<div class="col-md-3">
+		<div class="form-group name-group">
+			<label class="" for="vendor_image">Vendor Profile Image</label>
+			<div class="">
+				<input type="file" id="vendor_image" name="vendor_image" value="">
+			</div>
+		</div>
+	</div>
+	
+	<div class="col-md-6 catname-group">
+		<div class="form-group catname-group">
+			<label class="control-label" for="vendor_name">Vendor Name</label>
+				<input type="text" <?php echo TypeLength(); ?> id="vendor_name" name="vendor_name" class="form-control" placeholder="Category Name" value="<?php echo $CategeryData['category_name'] ?? null; ?>">
+		</div>
+	</div>
+ 
+
+<div class="col-md-6">
+    <div class="form-group name-group">
+        <label class="control-label" for="cat_slug">Category Slug</label>
+            <input type="text" <?php echo TypeLength(); ?> id="cat_slug" name="cat_slug" class="form-control" placeholder="Category Slug" value="<?php echo $CategeryData['category_slug'] ?? null; ?>">
+    </div>
+</div>
+
+<div class="col-md-6">
+     <div class="form-group name-group">
+        <label class="control-label" for="cat_salecaption">Sale Caption</label>
+       
+            <input type="text" <?php echo TypeLength(); ?> id="cat_salecaption" name="cat_salecaption" class="form-control" placeholder="Sale Caption" value="<?php echo $CategeryData['category_salecaption'] ?? null; ?>">
+
+    </div>
+</div>
+
+<div class="col-md-6">
+        
+         <label class="control-label" for="cat_sortorder">Sort Order</label>
+        
+            <input type="text" id="cat_sortorder" name="cat_sortorder" class="form-control" placeholder="Sort Order" value="<?php echo $CategeryData['category_sort'] ?? null; ?>">
+        
+    </div>
+
+
+<div class="col-md-3">
+	<div class="form-group">
+		<label for=""></label>
+		<div class="custom-control custom-checkbox">
+          <input type="checkbox" <?php echo isset($CategeryData['category_status']) &&  $CategeryData['category_status'] == 1 ? 'checked' : null; ?>  name="cat_status" value="1">
+          <label for="customCheckbox2" class="">Category Status</label>
+        </div>
+		  
+                        
+	</div>
+</div>
+
+
+<div class="col-md-3">
+	<div class="form-group">
+		<label for=""></label>
+		<div class="custom-control custom-checkbox">
+          <input type="checkbox"   name="cat_displayhome" value="1" <?php echo isset($CategeryData['category_displayhome']) &&  $CategeryData['category_displayhome'] == 1 ? 'checked' : null; ?>>
+          <label for="customCheckbox1" class="">Display On Home</label>
+        </div>               
+	</div>
+</div>
+
+<div class="col-md-12">
+	<div class="form-group">
+	 <label class="control-label" for="cat_shortdesc">Short Description</label>
+  
+      <textarea id="cat_shortdesc" name="cat_shortdesc" rows="3" class="form-control" placeholder="Short Description" <?php echo TypeLength(490); ?>><?php echo $CategeryData['category_shortdesc'] ?? null; ?></textarea>
+    </div>
+</div>
+
+
+<div class="col-md-12">
+	<div class="form-group">
+  <label class="control-label" for="cat_longdesc">Long Description</label>
+      
+    <textarea id="textarea-ckeditor" rows="7" name="cat_longdesc" class="textarea form-control" placeholder="Long Description"><?php echo isset($CategeryData['category_longdesc']) ? stripslashes($CategeryData['category_longdesc']) : null; ?></textarea>
+        </div>
+    
+</div>
+
+
+
+<div class="col-md-12">
+	  <div class="form-group name-group">
+            <label class="control-label" for="metatitle">Meta Title</label>
+                <input type="text" <?php echo TypeLength(); ?> id="metatitle" name="metatitle" class="form-control" placeholder="Meta Title" value="<?php echo $CategeryData['title'] ?? null; ?>">
+        </div>
+	</div>
+
+	<div class="col-md-12">
+		   <div class="form-group name-group">
+                        <label class="control-label" for="metadesc">Meta Description</label>
+                        
+                          <textarea <?php echo TypeLength(); ?> id="metadesc" name="metadesc" rows="3" class="form-control" placeholder="Meta Description"><?php echo $CategeryData['description'] ?? null; ?></textarea>
+                        
+                    </div>
+	</div>
+
+	<div class="col-md-12">
+		  <div class="form-group name-group">
+                        <label class="control-label" for="metakeyword">Meta Keyword</label>
+                        
+                          <textarea <?php echo TypeLength(); ?> id="metakeyword" name="metakeyword" rows="3" class="form-control" placeholder="Meta Keyword"><?php echo $CategeryData['keyword'] ?? null; ?></textarea>
+                        
+                    </div>
+	</div>
+</div>
+                   
+
+
+                   
+                     
+
+                   
+ 
+               <input type="hidden" name="cat_id" id="cat_id" value="<?php echo $CategeryData['category_id'] ?? null; ?>">
+                    <div class="form-group form-actions">
+                        <div class="col-md-9">
+<button type="submit" class="btn btn-primary">SUBMIT 
+              &nbsp; <svg style="display: none;" class="spinner" width="30" height="30" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+   <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+</svg>
+</button>
+                        </div>
+                    </div>
+                
+                </div>
+             
+              </form>
+            </div>
+            <!-- /.card -->
+
+
+          </div>
+
+        
+    <div class="clearfix"></div>
+
+    
+</div>
+ 
+
+
+
+<?php 
+  include('bottom.php');
+  include('footer.php');
+?>
+<script src="<?php echo $SiteUrl; ?>admin/js/vendor.js"></script>
